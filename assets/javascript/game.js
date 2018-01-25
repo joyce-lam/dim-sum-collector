7462f963ed5c6bb349156f2c0098c57386fd02f9
Joyce <joycelamsaungan@gmail.com>
2018-01-24 17:04:01 -0800

    modifying index.html, adding style.css images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,70 @@
+var targetScore;
+var counter;
+var wins = 0;
+var losses = 0;
 
-$(document).ready(function() {
+function genTargetScore() {
+	var min = 19;
+	var max = 120;
+	targetScore = Math.floor(Math.random()*(max - min +1))+ min;
+}
 
+function genButtonScore() {
+	var min = 1;
+	var max = 12;
+	return Math.floor(Math.random()*(max - min +1))+ min;
+}
 
-	var targetScore;
-	var counter = 0;
-	var wins = 0;
-	var losses= 0;
-
-
-	function genTargetScore() {
-		var min = 19;
-		var max = 120;
-		targetScore = Math.floor(Math.random()*(max - min +1))+ min;
+var buttonScores = [];
+function allButtonScores() {
+	for (var i = 0; i<4; i++) {
+		buttonScores[i] = genButtonScore();
 	}
+}
 
+function setButtonScores() {
+	$('.dimsum-image').each(function(index) {
+		$(this).data('dimsumvalue', buttonScores[index]);
+	});
+}
 
-	genTargetScore()
+function updateScore() {
+	$("#win").text(wins);
+	$("#loss").text(losses);
 	$("#targetscore").text(targetScore);
+	$("#totalscore").text(counter);
+}
 
-
-	function genButtonScore() {
-		var min = 1;
-		var max = 12;
-		return Math.floor(Math.random()*(max - min +1))+ min;
-	}
-
-
-	var buttonScores = [];
-	function allButtonScores() {
-		for (var i = 0; i<4; i++) {
-			buttonScores[i] = genButtonScore();
-		}
-	}
-
-
+function resetGame() {
+	counter = 0;
+	genTargetScore();
 	allButtonScores();
+	setButtonScores();
+	updateScore();
+}
 
 
-	var imageSources = [
-		"assets/images/gyoza.png",
-		"assets/images/porkbun.jpg",
-		"assets/images/siumai.jpg",
-		"assets/images/soupdumpling.jpg"
-	]; 
+function initGame() {
+	resetGame();
 
-
-	for (var i = 0; i<4; i++){
-		var dimsumImage = $("<img>");
-		dimsumImage.addClass("dimsum-image");
-		dimsumImage.attr("src", imageSources[i]);
-		dimsumImage.data('dimsumvalue', buttonScores[i]);
-		$("#dim-sums").append(dimsumImage);
-	}
-
-
-	function updateScore() {
-		$("#win").text(wins);
-		$("#loss").text(losses);
-		$("#totalscore").text(counter);
-	}
-
-
-	$(".dimsum-image").on("click", function(){
-
+	$(".dimsum-image").on("click", function() {
 		var dimsumValue = ($(this).data("dimsumvalue"));
 		dimsumValue = parseInt(dimsumValue);
 		counter += dimsumValue;
 
 		if (counter === targetScore) {
 			wins++;
-			$("#text").text("You won!")
-	    }
-	    else if (counter >= targetScore) {
+			$("#text").text("You won!");
+			resetGame();
+	    } else if (counter >= targetScore) {
 	    	losses++;
 	    	$("#text").text("You lost!")
-	    }
-	    updateScore();
-
+	    	resetGame();
+	    } else {
+		    updateScore();
+		}
 	});
+}
 
-})
+$(document).ready(initGame);
 
